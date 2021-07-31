47a51a1f32d34ee485778357ea786bd126edd710
Yeh Shuyung <>
2021-07-31 16:18:10 +0800

    test

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,13 +1,21 @@
 import json
 import os
+import subprocess
 
 
-def main():
+def main(event):
+    """
+    :param event: github event, https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types
+    :return:
+    """
     print(f'env: {os.environ}')
+    this_event = event['event']
+    diff_files = subprocess.check_output(['git', '--no-pager', 'diff', '--name-only', this_event['before'], this_event['after']])
+    print(diff_files)
 
 
 if __name__ == '__main__':
     with open('event.json') as event_fd:
         event = json.loads(event_fd.read())
     print(event)
-    main()
+    main(event)
