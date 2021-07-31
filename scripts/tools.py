ff6631160de3603e7f2bf9d1fcee7ffda013de8b
Yeh Shuyung <>
2021-07-31 19:23:25 +0800

    change workspace

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,5 +1,5 @@
 import json
-# import os
+import os
 import subprocess
 
 
@@ -21,23 +21,19 @@ def main(event):
     :return:
     """
     # print(f'env: {os.environ}')
-    this_event = event['event']
     event_name = event['event_name']
     event_name_to_sha_range = {'pull_request': get_pull_request_sha_range, 'pust': get_push_sha_range}
     if event_name not in event_name_to_sha_range:
         raise ValueError(event_name)
     get_sha_range = event_name_to_sha_range[event_name]
     base_sha, this_sha = get_sha_range(event)
-    git_log = subprocess.check_output(['git', '--no-pager', 'log'])
-    print(git_log.decode())
-    diff_files = subprocess.check_output(['git', '--no-pager', 'diff', '--name-only'])
-    print(diff_files.decode())
     diff_files = subprocess.check_output(['git', '--no-pager', 'diff', '--name-only', base_sha, this_sha])
-    print(diff_files.decode())
+    print(diff_files.decode().split())
 
 
 if __name__ == '__main__':
-    with open('event.json') as event_fd:
+    context = os.path.join(os.environ['GITHUB_WORKSPACE'], 'job_context')
+    with open(os.path.join(context, 'event.json')) as event_fd:
         event = json.loads(event_fd.read())
     # print(event)
     main(event)
